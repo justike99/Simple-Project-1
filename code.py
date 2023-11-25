180a93f397c98abbc92565bd64efc94ac8332aa4
justike99 <151986581+justike99@users.noreply.github.com>
2023-11-25 13:12:07 +0700

    Create code.py

---
+++ code.py
@@ -0,0 +1,4 @@
+print("Welcome to the Girl Band Name Generator")
+Name_Flower = input("What kind of flower do you like the most?\n")
+Name_Dessert = input("What's your favorite dessert?\n")
+print("Your band name could be \n"+ ">>>" + Name_Flower +" "+ Name_Dessert)  
